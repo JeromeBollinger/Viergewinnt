b6f3afd5a414587fd012379c11a1b8d8513e98b4
JeromeBollinger <jerome.f.bollinger@gmail.com>
2021-11-19 15:19:54 +0100

    bevore

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,10 @@ const int mux_S2 = 26;
 const int mux_S3 = 17;
 
 
+const float steps_per_rotation = 200;
+const float spindle_pitch = 1.25;
+const float coin_height = 35;
+
 
 void error_message(string error_m){
 	cout<<error_m<<endl;
@@ -35,58 +39,65 @@ void console(string text){
 
 
 
-bool set_mux_entrance(int output){
+bool set_mux_entrance(int in){
 
-int S0 = 0;
-int S1 = 0;
-int S2 = 0;
-int S3 = 0;
+	int S0 = 0;
+	int S1 = 0;
+	int S2 = 0;
+	int S3 = 0;
+	int output = in;
 
 	stringstream ss;
 	string str;
 	
-	ss << output;
+	ss << in;
 	ss >> str;
 	
-	if(output > 15 || output < 0) return false;
+	if(in > 15 || in < 0) return false;
 	
-	output++;
 	
 	if(output>=8){
 		S3 = 1;
 		output = output-8;
-		digitalWrite(S3, HIGH);
+		digitalWrite(mux_S3, HIGH);
+	}
+	else{
+		digitalWrite(mux_S3, LOW);
 	}
 	if(output>=4){
-		S3 = 1;
+		S2 = 1;
 		output = output-4;
-		digitalWrite(S2, HIGH);
+		digitalWrite(mux_S2, HIGH);
+	}
+	else{
+		digitalWrite(mux_S2, LOW);
 	}
 	if(output>=2){
-		S3 = 1;
+		S1 = 1;
 		output = output-2;
-		digitalWrite(S1, HIGH);
+		digitalWrite(mux_S1, HIGH);
+	}
+	else{
+		digitalWrite(mux_S1, LOW);
 	}
 	if(output>=1){
-		S3 = 1;
+		S0 = 1;
 		output = output-0;
-		digitalWrite(S0, HIGH);
+		digitalWrite(mux_S0, HIGH);
+	}
+	else{
+		digitalWrite(mux_S0, LOW);
 	}
 
 	
 	console("set mux" + str);
 	
-	stringstream ss0;
-	string str0;
-	ss0 << S0;
-	ss0 >> str0;
-	console("S0: " + str0);
 	
-	stringstream ss1;
-	string str1;
-	ss1 << S1;
-	ss1 >> str1;
-	console("S1: " + str1);
+	stringstream ss3;
+	string str3;
+	ss3 << S3;
+	ss3 >> str3;
+	console("S3: "+str3);
 	
 	stringstream ss2;
 	string str2;
@@ -94,11 +105,19 @@ int S3 = 0;
 	ss2 >> str2;
 	console("S2: "+str2);
 	
-	stringstream ss3;
-	string str3;
-	ss3 << S3;
-	ss3 >> str3;
-	console("S3: "+str3);
+	stringstream ss1;
+	string str1;
+	ss1 << S1;
+	ss1 >> str1;
+	console("S1: " + str1);
+	
+	stringstream ss0;
+	string str0;
+	ss0 << S0;
+	ss0 >> str0;
+	console("S0: " + str0);
+	
+	
 	
 	return true;
 	
@@ -193,7 +212,8 @@ int main(int argc, char** argv) {
 	pinMode(mux_S2, OUTPUT);
 	pinMode(mux_S3, OUTPUT);
 	
-	set_mux_entrance(10);
+	
+	
 	
 	//setup_lcd();
 	//setup():
